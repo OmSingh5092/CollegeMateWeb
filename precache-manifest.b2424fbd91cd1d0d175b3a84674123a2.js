44978a1c0d8206ce67e5e5c18617ef231485915e
OmSingh <omsingh6903@gmail.com>
2020-06-27 16:05:42 +0530

    First Commit

---
+++ precache-manifest.b2424fbd91cd1d0d175b3a84674123a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8e3289a616e0e44a27822531af46c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24487e573fad167afa90",
+    "url": "/static/css/main.0523755f.chunk.css"
+  },
+  {
+    "revision": "80e63895a9dbe22ea621",
+    "url": "/static/js/2.f785e8d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f785e8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24487e573fad167afa90",
+    "url": "/static/js/main.cec4e65e.chunk.js"
+  },
+  {
+    "revision": "b46329c21e1b6d0228a6",
+    "url": "/static/js/runtime-main.0e9e2167.js"
+  },
+  {
+    "revision": "46818ebd4f76c4e6fe9b030dbc74f5cf",
+    "url": "/static/media/Raleway-Black.46818ebd.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/static/media/Raleway-Medium.bb5ae98e.ttf"
+  }
+]);
